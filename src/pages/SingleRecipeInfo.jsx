131e787e77213c503991eabe1844f7a1be3f9a92
Artaza <artazakhan556@gmail.com>
2025-06-06 14:51:32 +0530

    fixing

--- src/pages/SingleRecipeInfo.jsx
+++ src/pages/SingleRecipeInfo.jsx
@@ -1,6 +1,6 @@
 import React, { useContext } from 'react'
 import { Link, useNavigate, useParams } from 'react-router-dom'
-import { recipeContext } from '../context/recipeContext';
+import { recipeContext } from '../context/RecipeContext';
 
 const SingleRecipeInfo = () => {
   let params = useParams()
